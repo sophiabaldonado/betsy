96e49537ee74d252ece29dda006e312a041a20ce
sophia <sophiabaldonado@gmail.com>
2016-05-04 21:42:51 -0700

    Migrate and fix seed file;

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505041208) do
+ActiveRecord::Schema.define(version: 20160505043619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "quantity"
+    t.string   "name"
+    t.decimal  "price"
+    t.string   "status"
+    t.integer  "order_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string   "status"
