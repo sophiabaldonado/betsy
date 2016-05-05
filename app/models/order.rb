adb10dff2ef7828a4de5ae734a6fd697b87a344c
sophia <sophiabaldonado@gmail.com>
2016-05-05 14:28:38 -0700

    Add validations for Orders and OrderItems models;

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :user
-  # I don't think we set up the schema right for the billing/orders relationship
   has_one :billing
   has_many :order_items
+
+  validates :status, presence: true
+  validates :confirmation_date, presence: true
+  validates :billing_id, presence: true
+  validates :order_number, presence: true, uniqueness: true
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,12 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
+  belgons_to :product
+
+  validates :quantity,   presence: true, numericality: { greater_than: 0 }
+  validates :name,       presence: true
+  validates :price,      presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :status,     presence: true
+  validates :order_id,   presence: true
+  validates :product_id, presence: true
+
 end
