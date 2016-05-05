b9b71b335d27a4c26cb99c4b7b3798ddf4179cff
sophia <sophiabaldonado@gmail.com>
2016-05-04 21:46:36 -0700

    Add relationship between Order and OrderItem;

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ActiveRecord::Base
   belongs_to :user
   # I don't think we set up the schema right for the billing/orders relationship
   has_one :billing
+  has_many :order_items
 end
