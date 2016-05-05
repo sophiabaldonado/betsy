53e6a6c52fa2d3cdcd9863f4b2230dfe15d3e80c
Cristal Tay <bluecolorpencils@gmail.com>
2016-05-05 14:28:21 -0700

    CT finished up with product, categories, and bridge model validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
-
+  belongs_to :order_item
+  belongs_to :user
+  has_many :categories, :through => :product_categories
+  has_many :reviews
+  validates :name, presence: true, uniqueness: true, length: { in: 1..30 }
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
 end
