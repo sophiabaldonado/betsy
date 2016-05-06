97f17a4e99e7de175ca3f338521e88f7898b943f
Cristal Tay <bluecolorpencils@gmail.com>
2016-05-06 13:46:26 -0700

    trying to get form_tag to actually add to the cart. it's not. damn it.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,14 +5,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id]) || Product.find(@product.id)
+    @product = Product.find(params[:id])
     @cart_items = CartItem.new
     @cart_items = CartItem.all
   end
 
   def create_cart
-    @cart_items = CartItem.new
+    @cart_items = CartItem.new(cart_params[:cart_item])
     @product = Product.find(params[:id]) || Product.find(@product.id)
+    raise
     if @cart_items.save
       redirect_to product_path(@product.id), :notice => 'Products added to cart.'
     else
@@ -30,7 +31,7 @@ class ProductsController < ApplicationController
   private
 
   def cart_params
-    params.require(:cart_item).permit(:quantity, :product_id)
+    params.permit(:quantity, :product_id)
 
   end
 end
