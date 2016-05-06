4cfc746b11cdd6c32391fa0d34043c1a2d23c892
sophia <sophiabaldonado@gmail.com>
2016-05-06 15:20:25 -0700

    Order create subtracts the correct quantity from inventory;

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,6 +30,7 @@ class OrdersController < ApplicationController
       @cart_items.each do |item|
         @order_item = OrderItem.new(quantity: item.quantity, name: item.product.name, price: item.product.price, status: "pending", order_id: @order.id, product_id: item.product.id)
         @order_item.save
+        @order_item.product.update(inventory: @order_item.product.inventory - @order_item.quantity)
       end
       redirect_to order_path(@order.id)
     else
