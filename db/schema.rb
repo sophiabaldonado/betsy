ad05444eee103ba2fb5c4ad15798e230b32cdaba
Cristal Tay <bluecolorpencils@gmail.com>
2016-05-12 15:04:42 -0700

    removed yes no merchant on yo's sign up page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160512205048) do
 
   # These are extensions that must be enabled in order to support this database
@@ -48,7 +47,6 @@ ActiveRecord::Schema.define(version: 20160512205048) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "photo_url"
   end
 
   create_table "order_items", force: :cascade do |t|
