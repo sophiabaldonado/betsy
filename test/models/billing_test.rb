194a809b2aaed7a660759475f7a9624db943c98f
Mindy Carson <mcarson1111@gmail.com>
2016-05-12 19:38:22 -0700

    testing for billing

--- test/models/billing_test.rb
+++ test/models/billing_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class BillingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  valid_params = {
+    first_name: "Bentley",
+    last_name: "Schreifels",
+    cc: 2222222222222222,
+    cvv: 222,
+    email: "bentley@cutie.com",
+    billing_zip: 98118,
+    address: "cutie street",
+    city: "Seattle",
+    state: "WA",
+    zip: 98118,
+    expiration_date: "09/16"
+  }
+
+  test "can create a valid billing" do
+    billing = Billing.new(valid_params)
+    billing.valid?
+    # puts '!!!!!!!!!!!!'
+    # puts billing.errors.full_messages
+    # puts '!!!!!!!!!!!!'
+
+    assert billing.valid?
+  end
+
+  test "Test that address is required" do
+    invalid_params = valid_params.clone
+    invalid_params[:address] = nil
+    billing = Billing.new(invalid_params)
+    billing.valid?
+    # puts '!!!!!!!!!!!!'
+    # puts billing.errors.full_messages
+    # puts '!!!!!!!!!!!!'
+    assert billing.errors.include?(:address)
+  end
+
 end
