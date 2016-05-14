ed526dc525df1df97378bea9a14797a6570bb10d
Cristal Tay <bluecolorpencils@gmail.com>
2016-05-13 21:48:28 -0700

    finished testing for product and categories

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,7 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a product has many categories" do
+    assert_equal 2, products(:p_one).categories.count
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -5,7 +5,25 @@ class ProductTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "product must belong to a user" do
-    assert_equal products(:p_one).user_id, users(:mike).user_id
+    assert_equal products(:p_one).user_id, users(:mike).id
   end
+
+  test "a retired product should be true" do
+    prod1 = Product.find(1)
+    prod1.update(retired: true)
+    assert_equal prod1.retired, true
+  end
+
+  test "if photo_url is nil, product_pic method saves with default" do
+    prod1 = Product.find(1)
+    prod1.update(photo_url: nil)
+    assert_equal prod1.product_pic, "http://www.apheresis.org/global_graphics/default-store-350x350.jpg"
+  end
+
+  test "if photo_url exists, product_pic method returns existing url" do
+    prod1 = Product.find(1)
+    assert_equal prod1.product_pic, "http://www.shoes.com"
+  end
+
 
 end
