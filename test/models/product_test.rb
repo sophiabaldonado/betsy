e7898436d55ee443e24915589300ccc92613c91a
Cristal Tay <bluecolorpencils@gmail.com>
2016-05-13 19:24:35 -0700

    yaml file for products up in hur.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,9 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product must belong to a user" do
+    assert_equal products(:p_one).user_id,
+  end
+
+  end
 end
