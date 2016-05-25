e2d6c6685509abe396bb41341da1e785ad44a000
Jillian Boshart <Jilliliz@gmail.com>
2016-05-25 09:17:35 -0700

    Added more info to our body in our ShippingServiceWrapper.

--- lib/ShippingServiceWrapper.rb
+++ lib/ShippingServiceWrapper.rb
@@ -5,18 +5,23 @@ class ShippingServiceWrapper
 
   #Do we need an initializer? Do we need new instances of ShippingService OR will the fancy gem ActiveShipping take care of that for us?
 
-  def self.get_quote(shipping_info, package_info)
+  def self.get_quote(destination_info, package_info)
     #Is this a get or a post? We're not sure yet, but you must know, future smarter self! Look how great you are!
 
     # Sending: Country, State, City, Zip
     # Sending: Weight, Height, Weight, Units (set to imperial as 'units: :imperil')
     @quotes = HTTParty.post(BASE_URL + "shipping/search",
     body: {
-      shipping_info: {
-        country: "#{shipping_info[:country]}",
-        state: "#{shipping_info[:state]}",
-        city: "#{shipping_info[:city]}",
-        zip: "#{shipping_info[:zip]}" },
+      origin_info: {
+        country: "United States",
+        state: "Washington",
+        city: "Seattle",
+        zip: "98103" },
+      destination_info: {
+        country: "#{destination_info[:country]}",
+        state: "#{destination_info[:state]}",
+        city: "#{destination_info[:city]}",
+        zip: "#{destination_info[:zip]}" },
       package_info: {
         weight: "#{package_info[:weight]}",
         height: "#{package_info[:height]}",
