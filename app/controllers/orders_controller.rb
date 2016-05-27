4fec2c8b5f8df9e80340398e99230ce6562c1b5b
Jillian Boshart <Jilliliz@gmail.com>
2016-05-27 14:33:01 -0700

    Final refactoring and code clean up.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,6 @@ class OrdersController < ApplicationController
   include OrdersHelper
   skip_before_action :require_login, only: [:new, :update_cart, :destroy, :create, :show, :shipping, :update_cart_with_shipping]
 
-
   def index
     if params[:user_id].to_i == current_user.id
       @user = User.find(current_user.id)
@@ -17,12 +16,15 @@ class OrdersController < ApplicationController
       else
         @orders = Order.where(user_id: @user.id)
       end
+
       @orders.each do |x|
         if Time.now.to_i > (x.updated_at.to_i + 172800)
           x.update(:status => "paid")
         end
       end
+
       params[:status] == "all orders" || params[:status].nil? ? @display_orders = @orders : @display_orders = orders_by_status(@orders, params[:status])
+
       @total = all_orders_revenue(@display_orders)
       @statuses = ["all orders", "paid", "pending", "complete", "cancelled"]
       @status = params[:status] if params[:status]
@@ -35,20 +37,15 @@ class OrdersController < ApplicationController
   def show
     #if they're a merchant:
     if current_user && "/users/#{current_user.id}/sold/#{params[:id]}" == request.env['PATH_INFO']
-      @order = Order.find(params[:id])
-      #@user = User.find(session[:user_id])
+      @order = Order.find(session[:order_id])
       @order_items = OrderItem.where(:product_id => @user.products)
     elsif current_user.nil? # if they're a customer
       @order = Order.find(session[:order_id])
       @order_items = OrderItem.where(:order_id => @order.id)
 
     else # if they're a customer
-      @order = Order.find(params[:user_id])
-      # if current_user
-      #   @user = User.find(session[:user_id])
-      # end
+      @order = Order.find(session[:order_id])
       @order_items = OrderItem.where(:order_id => @order.id)
-
     end
   end
 
@@ -60,48 +57,41 @@ class OrdersController < ApplicationController
   end
 
   def create
-    # shipping = sprintf('%.2f', (params["order"]["shipping_rate"].to_i / 100.0))
     shipping = params["order"]["shipping_rate"].to_i
     @billing = Billing.new
+
     if @billing.save(validate: false)
+
       if current_user
         @cart_items = CartItem.all.where(user_id: session[:user_id])
       else
         @cart_items = CartItem.all.where(session_id: session[:session_id])
       end
+
       @user_id = session[:user_id] if session[:user_id]
       @billing_id = @billing.id
       @order_number = order_number
+
       @order = Order.find(session[:order_id])
       total_in_pennies = (total_order_revenue(@cart_items) * 100)
-      @order.update(status: "pending", total: (total_in_pennies + shipping + (total_in_pennies * 0.1)), confirmation_date: Time.now, order_number: @order_number, billing_id: @billing_id, user_id: @user_id)
-      @order.update(shipping_rate: params["order"]["shipping_rate"])
+      @order.update(status: "pending", total: (total_in_pennies + shipping + (total_in_pennies * 0.1)), confirmation_date: Time.now, order_number: @order_number, billing_id: @billing_id, user_id: @user_id, shipping_rate: params["order"]["shipping_rate"])
+
       @rates = nil
-      if @order.save
-        @cart_items.each do |item|
-          @order_item = OrderItem.new(quantity: item.quantity, name: item.product.name, price: item.product.price*item.quantity, status: "pending", order_id: @order.id, product_id: item.product.id)
-          @order_item.save
-          item.destroy
-          @order_item.product.update(inventory: @order_item.product.inventory - @order_item.quantity)
-        end
-    #     if current_user
-    #       redirect_to user_order_path(current_user.id, @order.id)
-    #     else
-    # raise
-    #       # redirect_to checkout_confirmation_path(@order.id)
-    #     end
-      else
-        render "/billings/new"
+
+      @cart_items.each do |item|
+        @order_item = OrderItem.new(quantity: item.quantity, name: item.product.name, price: item.product.price*item.quantity, status: "pending", order_id: @order.id, product_id: item.product.id)
+        @order_item.save
+        item.destroy
+        @order_item.product.update(inventory: @order_item.product.inventory - @order_item.quantity)
       end
     end
-        render "/billings/new"
+    render "/billings/new"
   end
 
   def update_cart
     @cart_item = CartItem.find(update_cart_params[:id])
     @cart_item.update(quantity: update_cart_params[:quantity])
     redirect_to action: "new"
-
   end
 
   def item_shipped
@@ -123,13 +113,13 @@ class OrdersController < ApplicationController
       @cart_items = CartItem.where(session_id: session[:session_id])
     end
 
-    #Currently hard coding each item to weigh a pound. Therefore quantity = weight.
+    #Each item to weigh a pound. Therefore, quantity = weight.
     @quantity = 0
     @cart_items.each do |item|
       @quantity += item.quantity
     end
 
-    #Currently hard coding the dimensions as SOMETHING so that we can move on to API.
+    #Every item comes in the same box:
     @packing_info = {
       weight: "#{@quantity}",
       height: "15",
@@ -138,6 +128,7 @@ class OrdersController < ApplicationController
     }
 
     @rates = ShippingServiceWrapper.get_quote(params, @packing_info).parsed_response
+
     # creating array for drop down menu in view
     @rates_array = []
     @rates[0].each do |option|
@@ -147,18 +138,9 @@ class OrdersController < ApplicationController
       @rates_array << ["#{option[0]}: #{number_to_currency(option[1])}", option[1]]
     end
 
-
     new_helper
     render :new
   end
-
-  # def update_cart_with_shipping
-  #   @order = Order.find(session[:order_id])
-  #   @order.update(shipping_rate: params["order"]["shipping_rate"])
-  #   @rates = nil
-  #   # create
-  #   redirect_to "/billings/new"
-  # end
 
 ###### PRIVATE STARTS HERE!!!!!!! ######
   private
@@ -171,10 +153,7 @@ class OrdersController < ApplicationController
     params.permit(OrderItem: [:shipped])
   end
 
-
   def billing_params
     params.permit(billing: [:first_name, :last_name, :cc, :cvv, :expiration_date, :email, :billing_zip, :address, :address2, :city, :state, :zip, :user_id])
   end
-
-
 end
